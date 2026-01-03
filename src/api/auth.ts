328e6ba5875edc53c879c7027f6b9425105fba8a
Kimcheez-cloud <eutychuskim@outlook.com>
2026-01-03 19:21:46 +0000

    Broken code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,13 +3,12 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    return null;
+    return "nothing";
   }
 
   const splitAuth = authHeader.split(" ");
   if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
-    return null;
+    return "nothing";
   }
-
   return splitAuth[1];
 }
